6da0115127031b88cd41942c50043f3e7079947a
Quentin Gibson <quentingibson94@gmail.com>
2022-09-26 13:09:07 -0400

    add profile image to about section

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import Hero from "./components/hero/Hero"
 import Navigation from './components/navigation/Navigation'
+import About from './components/about/About'
 
 export function App() {
   return (
@@ -7,6 +8,7 @@ export function App() {
       <div class="px-5">
         <Navigation />
         <Hero />
+        <About />
       </div>
     </>
   )
--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,14 +1,29 @@
+import './index.css'
+import large from '../../assets/personal-large.jpg'
+import med from '../../assets/personal-med.jpg'
+import small from '../../assets/personal-small.jpg'
 const About = () => (
   <>
-    <section>
-      <h3 class="after-line text-3xl text-gray-300 mb-10">About Me</h3>
-      <div class="flex flex-col">
+    <section class="mt-36">
+      <h3 class="font-bold after_line text-2xl text-gray-300 mb-10 flex whitespace-nowrap items-center">About Me</h3>
+      <div class="flex flex-col mb-2">
         <div class="flex flex-col">
           <p class="text-base text-gray-400 mb-4">Welcome to my site! My name is Quentin Gibson and I enjoy creating things using my programming skills. My interest in web development started back in 2012 when I decided to create a website for a family member. I enjoyed making that website and continued to program everyday since!</p>
-          <p>Today, I have had the privilege of working at a start-up, a huge-corporation, and as an freelancer. I have contributed to open source projects; Spacedrive, freecodecamp. Now I am focused on building my own open source project. </p>
-          <p>I am also working on a new website that help gamers! Stay tuned for more information</p>
-          <p>Here are a few technologies I've been working with recently:</p>
+          <p class="text-base text-gray-400 mb-4">Today, I have had the privilege of working at a start-up, a huge-corporation, and as an freelancer. I have contributed to open source projects; Spacedrive, freecodecamp. Now I am focused on building my own open source project. </p>
+          <p class="text-base text-gray-400 mb-4">I am also working on a new website that help gamers! Stay tuned for more information</p>
         </div>
+        <p class="text-base text-gray-400 mb-4">Here are a few technologies I've been working with recently:</p>
+      </div>
+      <ul class="skills-list">
+        <li class="mb-2.5 pl-5 text-base text-gray-400">Typescript</li>
+        <li class="mb-2.5 pl-5 text-base text-gray-400">Javascript (ES6)</li>
+        <li class="mb-2.5 pl-5 text-base text-gray-400">React</li>
+        <li class="mb-2.5 pl-5 text-base text-gray-400">Node.js</li>
+        <li class="mb-2.5 pl-5 text-base text-gray-400">Wordpress</li>
+        <li class="mb-2.5 pl-5 text-base text-gray-400">Rust</li>
+      </ul>
+      <div class="mt-5 flex h-[300px]">
+        <img class="h-full w-full object-contain" src={small} srcSet={`${small} 350w, ${med} 600w, ${large} 1000w`} alt="An image of me, Quentin Gibson." />
       </div>
     </section>
   </>
