d04ea9106357894109ab1b8e98d131ec2358e3ed
Quentin Gibson <quentingibson94@gmail.com>
2022-09-26 00:38:38 -0400

    create about component

---
+++ src/components/about/About.tsx
@@ -0,0 +1,16 @@
+const About = () => (
+  <>
+    <section>
+      <h3 class="after-line text-3xl text-gray-300 mb-10">About Me</h3>
+      <div class="flex flex-col">
+        <div class="flex flex-col">
+          <p class="text-base text-gray-400 mb-4">Welcome to my site! My name is Quentin Gibson and I enjoy creating things using my programming skills. My interest in web development started back in 2012 when I decided to create a website for a family member. I enjoyed making that website and continued to program everyday since!</p>
+          <p>Today, I have had the privilege of working at a start-up, a huge-corporation, and as an freelancer. I have contributed to open source projects; Spacedrive, freecodecamp. Now I am focused on building my own open source project. </p>
+          <p>I am also working on a new website that help gamers! Stay tuned for more information</p>
+          <p>Here are a few technologies I've been working with recently:</p>
+        </div>
+      </div>
+    </section>
+  </>
+)
+export default About
