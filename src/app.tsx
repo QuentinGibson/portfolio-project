fd00122677fa2fef2d45c625e2b728406522b5a9
Quentin Gibson <quentingibson94@gmail.com>
2022-09-27 13:03:33 -0400

    add projects component

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import Hero from "./components/hero/Hero"
 import Navigation from './components/navigation/Navigation'
 import About from './components/about/About'
 import Work from './components/work/Work'
+import Projects from './components/projects/Projects'
 
 export function App() {
   return (
@@ -11,6 +12,7 @@ export function App() {
         <Hero />
         <About />
         <Work />
+        <Projects />
       </div>
     </>
   )
---
+++ src/components/projects/Projects.tsx
@@ -0,0 +1,62 @@
+import GithubIcon from "~icons/icon-park/github"
+import ShareIcon from "~icons/icon-park/share"
+import ImageIcon from '~icons/fluent/image-28-regular'
+import Popup from 'reactjs-popup'
+import './index.css'
+import 'reactjs-popup/dist/index.css'
+
+const Projects = () => {
+  const projectList = [
+    {
+      featured: true,
+      image: '',
+      name: 'Chess Vision Trainer',
+      description: "A fullstack (MERN) app. A fun small game that tests your chess notation knowledge and allows you to share your score with the world through a leaderboard.",
+      stack: ["React", "Express", "Mysql", "Node.js", "Storybook", "react-router-dom"],
+      github: '',
+      live: ''
+    }
+  ]
+  const featureProjectsElements = projectList.filter(project => project.featured).map(project => {
+    const stackElement = project.stack.map(item => {
+      return (
+        <span class="text-gray-400">{item}</span>
+      )
+    })
+
+    return (
+      <li class="my-5">
+        <div class="shadow px-5 py-6 bg-cyan-900">
+          <span class="cyan text-sm mb-2">Featured Project</span>
+          <h4 class="text-gray-300 text-2xl">{project.name}</h4>
+          <Popup trigger={<div class="flex items-center justify-start"><button class="hover:underline-offset-4 hover:underline cyan text-base px-2 py-1 rounded bg-slate-900"> View <ImageIcon class="inline-flex" /> </button></div>} position="center center">
+            <img src={project.image} alt="Image of project" />
+          </Popup>
+          <p class="text-gray-300 my-3">{project.description}</p>
+          <div class="flex gap-3 flex-wrap">
+            {stackElement}
+          </div>
+          <div class="flex mt-4 gap-3">
+            <span><a class="icon-link" href={project.github}><GithubIcon class="fill-gray-300 text-xl" /></a></span>
+            <span><a class="icon-link" href={project.live}><ShareIcon class="fill-gray-300 text-gray-300 text-xl" /></a></span>
+          </div>
+        </div>
+      </li>
+    )
+  })
+
+  return (
+    <section class="my-20">
+      <h3 class="flex whitespace-nowrap items-center after_line text-gray-300 text-xl mb-10">Some Things I've Built</h3>
+      <div>
+        <div>
+          <ul>
+            {featureProjectsElements}
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Projects
